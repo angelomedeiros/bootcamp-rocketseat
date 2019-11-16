783d8c16f851364bf90273ee333f318ea09bbb2d
Angelo Medeiros Nóbrega <angelomedeiros3@gmail.com>
2019-11-16 12:57:18 -0300

    Creating loader of the models

--- gobarber/backend/src/routes.js
+++ gobarber/backend/src/routes.js
@@ -1,9 +1,17 @@
 import { Router } from "express";
 
+import User from "./app/models/User";
+
 const routes = new Router();
 
-routes.get("/", (req, res) => {
-  return res.json({ message: "Hello world" });
+routes.get("/", async (req, res) => {
+  const user = await User.create({
+    name: "Angelo Medeiros",
+    email: "angelomedeiros@email.com",
+    password_hash: "123123123"
+  });
+
+  return res.json(user);
 });
 
 export default routes;
