5b0e4293a5b45b8bc7e567879253edd59c38e251
Angelo Medeiros Nóbrega <angelomedeiros3@gmail.com>
2019-11-16 19:24:51 -0300

    Adding validations in create and update user

--- gobarber/backend/src/app/controllers/UserController.js
+++ gobarber/backend/src/app/controllers/UserController.js
@@ -1,7 +1,26 @@
+import * as Yup from "yup";
+
 import User from "../models/User";
 
 class UserController {
   async store(req, res) {
+    const schema = Yup.object().shape({
+      name: Yup.string().required(),
+      email: Yup.string()
+        .email()
+        .required(),
+      password: Yup.string()
+        .min(6)
+        .required()
+    });
+
+    if (!(await schema.isValid(req.body))) {
+      const errors = await schema.validate(req.body, { abortEarly: false }).catch(err => err.errors);
+      return res.status(400).json({
+        error: errors
+      });
+    }
+
     const userExists = await User.findOne({
       where: {
         email: req.body.email
@@ -23,6 +42,29 @@ class UserController {
   }
 
   async update(req, res) {
+    const schema = Yup.object().shape({
+      name: Yup.string(),
+      email: Yup.string().email(),
+      oldPassword: Yup.string().min(6),
+      password: Yup.string()
+        .min(6)
+        .when("oldPassword", (oldPassword, field) => (oldPassword ? field.required() : field)),
+      confirmPassword: Yup.string().when("password", (password, field) =>
+        password
+          ? field
+              .required()
+              .oneOf([Yup.ref("password")], "The password field does not match with confirm password field")
+          : field
+      )
+    });
+
+    if (!(await schema.isValid(req.body))) {
+      const errors = await schema.validate(req.body, { abortEarly: false }).catch(err => err.errors);
+      return res.status(400).json({
+        error: errors
+      });
+    }
+
     const { email, oldPassword } = req.body;
 
     const user = await User.findByPk(req.userId);
@@ -42,7 +84,7 @@ class UserController {
     }
 
     if (oldPassword && !(await user.checkPassword(oldPassword))) {
-      return res.status(401).json({
+      return res.status(400).json({
         error: "Password does not match"
       });
     }
